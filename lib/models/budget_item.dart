1058799ddc3c3e091e39afa3564ed4a86d9dc9c3
CodingDashUU <washutshotheli8@gmail.com>
2026-04-25 21:35:13 +0200

    Added a fromJson method for BudgetItem at budget_item.dart

--- lib/models/budget_item.dart
+++ lib/models/budget_item.dart
@@ -4,6 +4,10 @@ class BudgetItem {
 
   const BudgetItem(this.name, this.amount);
 
+  BudgetItem.fromJson(Map<String, dynamic> json)
+      : name = json['name'] as String,
+        amount = (json['amount'] as num).toDouble();
+
   Map<String, dynamic> toJson() => {
     'name': name,
     'amount': amount,

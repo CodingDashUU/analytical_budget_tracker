72ed6475b880fd188ab81c2b9e92d37ac7804d61
CodingDashUU <washutshotheli8@gmail.com>
2026-04-27 21:58:42 +0200

    Added the GPLv3 license and the license header to almost all source files

--- lib/screens/analytics.dart
+++ lib/screens/analytics.dart
@@ -1,7 +1,23 @@
+// Copyright (C) 2026 CodingDashUU
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 import 'package:budget_tracker/state/analysis_model.dart';
 import 'package:budget_tracker/state/budget_info.dart';
 import 'package:budget_tracker/widgets/verdict_card.dart';
 import 'package:flutter/material.dart';
+
 import '../models/months.dart';
 import '../widgets/donut_chart.dart';
 import '../widgets/drawer.dart';
